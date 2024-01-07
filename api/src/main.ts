1b517dc975d862914f51fb618f20509ec5961353
Goomees <mateus.nicolau@devmagic.com.br>
2024-01-07 11:44:29 -0300

    feat(*): implement cors and add .env in nest api

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,13 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { config as ConfigurationENV } from 'dotenv';
+
+const Logguer = new Logger("MainLoader");
+
+ConfigurationENV();
 
 async function bootstrap() {
-  const LOG = new Logger("MainLoader");
   const PORT = process.env.PORT || 5000
-  
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
-  LOG.log(`Server running on port ${PORT}`);
+
+  app.enableCors({
+    origin: process.env.FRONT_END_URL,
+  });
+
+  await app.listen(PORT, () => 
+    Logguer.log(`Server running on port ${PORT}`)
+  );
+  
 }
+
 bootstrap();
