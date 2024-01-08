3baa02166af1b5b8ff56ccf5765f275f34e5dcf7
Goomees <mateus.nicolau@devmagic.com.br>
2024-01-08 14:51:31 -0300

    fix(*): refactor and extract dropzone inside UploadComponent

---
+++ app/src/html/components/DropzoneComponent.tsx
@@ -0,0 +1,52 @@
+import { useDropzone } from "react-dropzone";
+import { formartBytes } from "utils/conversor";
+
+import  './css/DropzoneComponent.css'
+
+interface iDropZoneAndInputFilesProperties {
+    onError: ({message}) => void
+    onDrop: any
+    title : string
+    maxSizeBytes ?: number
+}
+
+export default function DropZoneAndInputMovies({
+    onDrop, onError, 
+    title,
+    maxSizeBytes = 50 * 1024 * 1024
+}: iDropZoneAndInputFilesProperties) {
+
+    const { getRootProps, getInputProps } = useDropzone({
+        onDrop: onDrop,
+        accept: {
+            'video': ['video/*'],
+        },
+        maxSize: maxSizeBytes,
+        onDropRejected(fileRejections, event) {
+            fileRejections.forEach(fileRejection => {
+                switch (fileRejection.errors[0].code) {
+                    case 'file-too-large':
+                        onError({
+                            message: `O arquivo ${fileRejection.file.name} ultrapassa o limite de ${formartBytes(maxSizeBytes)}`
+                        })
+                        break;
+                    case 'file-invalid-type':
+                        onError({
+                            message: `O arquivo ${fileRejection.file.name} do tipo ${fileRejection.file.type} não é permitido`
+                        })
+                        break;
+                    default:
+                        break;
+                }
+
+                return;
+            })
+        },
+    });
+    return (
+        <div {...getRootProps()} className='container-dropzone-input'>
+            <input {...getInputProps()}/>
+            <span>{title}</span>
+        </div>
+    )
+}
