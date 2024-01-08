55edf457a4ce6f0550b63b61e47ffee6fba81509
Goomees <mateus.nicolau@devmagic.com.br>
2024-01-08 14:29:10 -0300

    fix(*): remove fake progreess

--- app/src/services/processAndConvertMovieWorker.ts
+++ app/src/services/processAndConvertMovieWorker.ts
@@ -27,22 +27,3 @@ onmessage = async ({ data }: Partial<iWorkerProperties>) => {
 
 
 export {}
-
-
-function fakeUpload(cb){
-  let progress = 0;
-  let done = false;
-  const idIntervalo = setInterval(() => {
-    progress += 5;
-  
-    // Verifica se a tarefa está completa
-    if (progress >= 100) {
-      done = true;
-      progress = 100; // Garante que o progresso não exceda 100
-      clearInterval(idIntervalo); // Limpa o intervalo para parar os updates
-    }
-    
-    // Enviar o progresso e o status de 'done' de volta para o thread principal
-    cb({ progress, done })
-  }, Math.random() * 5000);
-}
